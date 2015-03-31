1b38be81721b4725af1409386f07c89274041139
n10sit <johnmasters43@gmail.com>
2015-03-31 17:19:07 -0500

    starting to work on stock market minigame

---
+++ src/server/model/players/Company.java
@@ -0,0 +1,32 @@
+package server.model.players;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Company {
+	public Integer id;
+  	public Integer value;
+	public Integer shares;
+	public Double growthRate;
+  	public String name;
+
+  	Company() {}
+  	public Company(int id, int value, int shares, Double growthRate, String name) {
+  			this.id = id;
+    		this.value = value;
+    		this.shares = shares;
+    		this.growthRate = growthRate;
+    		this.name = name;
+  	}
+  	//4*3=12
+  	//12/3=4
+  	public static List<Company> companies;
+  	
+  	public static void setList() {
+  		companies = Arrays.asList(
+  					new Company(1, 100000000, 200000, 0.02, "Ordan's Ores"),
+  					new Company(2, 350000000, 700000, 0.02, "Bob's Axes")
+  					);
+  	}
+  	
+}
--- src/server/model/players/PlayerAssistant.java
+++ src/server/model/players/PlayerAssistant.java
@@ -110,6 +110,8 @@ public class PlayerAssistant{
 		}
 		return -1;
 	}
+	
+	
 
 	/**
 	 * MulitCombat icon
